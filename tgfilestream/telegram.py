723011739f629ea4f5a0f705a80443172f433733
Suyash Jain <53927129+Suyashjain05@users.noreply.github.com>
2020-09-01 08:51:39 +0530

    Update telegram.py

--- tgfilestream/telegram.py
+++ tgfilestream/telegram.py
@@ -37,26 +37,13 @@ transfer = ParallelTransferrer(client)
 
 @client.on(events.NewMessage)
 async def handle_message(evt: events.NewMessage.Event) -> None:
- if not evt.is_private:
+    if not evt.is_private:
         await evt.reply(group_chat_message)
         return
     if not evt.file:
         await evt.reply(start_message)
         return
     url = public_url / str(pack_id(evt)) / get_file_name(evt)
-
-
-API_KEY = '89e771c8e00dbba8bb36cdbaf4ef2bdf8fc2800f'
-async def get_shortlink(url): 
-    surl = 'https://gplinks.in/api'
-    params = {'api': API_KEY, 'surl': url}
-    
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True) as response:
-            data = await response.json()
-            return data["shortenedUrl"]
-
-
-
-
-   
+    await evt.reply(f"Link to download file: {url}")
+    log.info(f"Replied with link for {evt.id} to {evt.from_id} in {evt.chat_id}")
+    log.debug(f"Link to {evt.id} in {evt.chat_id}: {url}")
